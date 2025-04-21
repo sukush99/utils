2f82f11c8133a6af36d19e2e34dffba5c7b6cac9
sukush99 <sukushkunwar99@gmail.com>
2025-04-21 10:10:12 +1000

    to_tensor

--- viz.py
+++ viz.py
@@ -1,5 +1,8 @@
-import numpy as np
+import torch
+from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 def show_images(full_dataset, num_images):
     """
@@ -14,8 +17,9 @@ def show_images(full_dataset, num_images):
     indices = np.random.choice(len(full_dataset), num_images, replace=False)
     for i in indices:
         img, label = full_dataset[i]
+        img_tensor = to_tensor(img)
         print(f"Label: {label}")
-        npimg = img.numpy()
+        npimg = img_tensor.numpy()
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
         plt.axis('off')
         plt.show()
